8e3b19dedf6119fcba80cb0bdeb28d9c9075e823
Andy <rosario.andrew@gmail.com>
2014-11-01 09:47:21 -0500

    Playing with Dates

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-
+// Show/Hide Calendar
 $(document).on('click', '#checkAvail', function {
 	$('#checkAvail').toggleClass('.hidden')
 })
 
-
-	
+// Form Data
+function testResults (form) {
+    var TestVar = form.checkIn.value;
+    alert ("You typed: " + TestVar);
+}
