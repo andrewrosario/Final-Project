cdb086d64c6ef61fc88118edf326581d18948abb
Andy <rosario.andrew@gmail.com>
2014-11-01 14:15:04 -0500

    Dates start day of week and stay length accurate

--- js/app.js
+++ js/app.js
@@ -25,23 +25,29 @@ var rentalPrice = 0
 // }
 
 function getQuote(form) {
-	var fullDate = new Date(form.checkIn.value);
-	console.log(fullDate);
+	var fullDate = new Date(form.checkIn.value.substring(0,4), form.checkIn.value.substring(5,7)-1, form.checkIn.value.substring(8,10));
+	// console.log(form.checkIn.value.substring(0,4));
+	// console.log(form.checkIn.value.substring(5,7));
+	// console.log(form.checkIn.value.substring(8,10));
+	// console.log(form.checkIn.value);
+	// console.log(fullDate);
 	var firstDay = fullDate.getDay();
+	console.log("Day of the week is " + firstDay);
 	// var startDay = firstDay(this.form);
 	var d1 = new Date (form.checkIn.value);
 	var d2 = new Date (form.checkOut.value);
-	var numDays =(d2 - d1)/(60*24*60*1000);
-	console.log((d2 - d1)/(60*24*60*1000));
-	console.log(firstDay);
-	var stayLength = numDays(this.form);
-	console.log(stayLength);
-	for(i === startDay; i <= stayLength; i++) {
+	var numDays = (d2 - d1)/(60*24*60*1000);
+	console.log("The stay is " + numDays + " nights");
+	// var stayLength = numDays;
+	// console.log("stayLength is" + stayLength);
+	for(i = firstDay; i <= numDays; i++) {
 		if(i % 6 === 0 || i % 5 === 0) {
-			rentalPrice = rentalPrice + 10;
+			// rentalPrice = rentalPrice + 10
+			console.log(i)
 		}
 		else {
-			rentalPrice = rentalPrice +1;
+			// rentalPrice = rentalPrice +1;
+			console.log("Running")
 		}
 		console.log(rentalPrice);
 	}
