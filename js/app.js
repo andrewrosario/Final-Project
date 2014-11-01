6efd568874585be5e3fdc6f6bd8f91c2c437f267
Andy <rosario.andrew@gmail.com>
2014-11-01 12:47:37 -0500

    Replace boilerplate head and script tags with fdn

--- js/app.js
+++ js/app.js
@@ -3,12 +3,7 @@
 $(document).foundation();
 
 // Show/Hide Calendar
-$(document).on('click', '#checkAvail', function {
-	$('#checkAvail').toggleClass('.hidden')
+$(document).on('click', '#checkAvail', function() {
+	$('#checkAvail').toggleClass('.hidden');
 })
 
-// Form Data
-function testResults (form) {
-    var TestVar = form.checkIn.value;
-    alert ("You typed: " + TestVar);
-}
