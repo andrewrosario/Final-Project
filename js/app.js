4e1a8697bc51b94bb041450db7e0bf9204802905
Andy <rosario.andrew@gmail.com>
2014-11-01 14:31:27 -0500

    Rental Price For look working

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ var rentalPrice = 0
 // }
 
 function getQuote(form) {
+	rentalPrice = 0;
 	var fullDate = new Date(form.checkIn.value.substring(0,4), form.checkIn.value.substring(5,7)-1, form.checkIn.value.substring(8,10));
 	// console.log(form.checkIn.value.substring(0,4));
 	// console.log(form.checkIn.value.substring(5,7));
@@ -40,15 +41,17 @@ function getQuote(form) {
 	console.log("The stay is " + numDays + " nights");
 	// var stayLength = numDays;
 	// console.log("stayLength is" + stayLength);
-	for(i = firstDay; i <= numDays; i++) {
+	// console.log(firstDay);
+	// console.log(typeof(firstDay));
+	for(i = firstDay; i <= numDays+firstDay-1; i++) {
 		if(i % 6 === 0 || i % 5 === 0) {
-			// rentalPrice = rentalPrice + 10
-			console.log(i)
+			rentalPrice = rentalPrice + 10
+			// console.log(rentalPrice)
 		}
 		else {
-			// rentalPrice = rentalPrice +1;
-			console.log("Running")
+			rentalPrice = rentalPrice +1
+			// console.log(rentalPrice)
 		}
-		console.log(rentalPrice);
+		console.log("Rental Price is " + rentalPrice);
 	}
 }
