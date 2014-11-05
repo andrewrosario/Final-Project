6bfeef369395a530cb0ca6b5082866092ff42ae2
Andy <rosario.andrew@gmail.com>
2014-11-04 20:09:23 -0600

    remove quote div

--- js/app.js
+++ js/app.js
@@ -25,20 +25,22 @@ var rentalPrice = 0
 // }
 
 function getQuote(form) {
+	// Remove Old Quotes
+	$('.quoteWrapper').remove();
 	// Find Rental Rate
 	rentalPrice = 0;
 	var fullDate = new Date(form.checkIn.value.substring(6,10), form.checkIn.value.substring(0,2)-1, form.checkIn.value.substring(3,5));
-	console.log(form.checkIn.value.substring(3,5));
+	// console.log(form.checkIn.value.substring(3,5));
 	// console.log(form.checkIn.value.substring(5,7));
 	// console.log(form.checkIn.value.substring(8,10));
-	console.log(form.checkIn.value);
-	console.log(fullDate);
+	// console.log(form.checkIn.value);
+	// console.log(fullDate);
 	var firstDay = fullDate.getDay();
-	console.log("Day of the week is " + firstDay);
+	// console.log("Day of the week is " + firstDay);
 	var d1 = new Date (form.checkIn.value);
 	var d2 = new Date (form.checkOut.value);
 	var numDays = (d2 - d1)/(60*24*60*1000);
-	console.log("The stay is " + numDays + " nights");
+	// console.log("The stay is " + numDays + " nights");
 	// console.log("stayLength is" + stayLength);
 	// console.log(firstDay);
 	// console.log(typeof(firstDay));
@@ -58,12 +60,12 @@ function getQuote(form) {
 	}
 	// Find Extra Guests
 	var extraGuests = form.numGuests.value;
-	console.log(extraGuests);
-	console.log(form.checkIn.value);
-	console.log(form.checkOut.value);
+	// console.log(extraGuests);
+	// console.log(form.checkIn.value);
+	// console.log(form.checkOut.value);
 	var fullPrice = rentalPrice + 125 + ((extraGuests)*20);
-	console.log("The full price is $" + fullPrice.toFixed(2));
-	$('.button').after('<p>The full price is $' + fullPrice.toFixed(2) + '</p>');
+	// console.log("The full price is $" + fullPrice.toFixed(2));
+	$('.button').after('<div class="quoteWrapper"><p>The full price is $' + fullPrice.toFixed(2) + '</p></div>');
 	
 }
 
